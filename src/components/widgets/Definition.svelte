73af0b33c1b29b60807f389115985dfda07159e4
Dan <36249892+dante-militello@users.noreply.github.com>
2024-03-28 17:24:13 -0300

    https

--- src/components/widgets/Definition.svelte
+++ src/components/widgets/Definition.svelte
@@ -7,7 +7,7 @@
 
 	async function getWordData(word) {
     try {
-        const response = await fetch(`http://api.piword.piberio.com/buscar?palabra=amor`, {
+        const response = await fetch(`https://api.piword.piberio.com/buscar?palabra=amor`, {
             mode: "cors",
         });
 
