2f7fe26c8eb8c795453fc753ec4306e52914a9a8
Dan <36249892+dante-militello@users.noreply.github.com>
2024-03-28 19:52:29 -0300

    changed api domain

--- src/components/widgets/Definition.svelte
+++ src/components/widgets/Definition.svelte
@@ -7,7 +7,7 @@
 
 	async function getWordData(word) {
     try {
-        const response = await fetch(`https://api.piword.piberio.com/buscar?palabra=amor`, {
+        const response = await fetch(`https://apipiword.piberio.com/buscar?palabra=amor`, {
             mode: "cors",
         });
 
